3c409b6b5c83ed5a10196c54c207f9ebf61f7f95
ABiondic <antoniobiondic05@gmail.com>
2026-04-27 13:36:21 +0200

    Update LV2_Biondic_zad3.py

--- Biondic_PSU/LV2-PSU/LV2_Biondic_zad3.py
+++ Biondic_PSU/LV2-PSU/LV2_Biondic_zad3.py
@@ -1,11 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-img = plt.imread("tiger.png")
-img = img.rotate(90)
-print(img.shape)
-print(img.dtype)
-plt.figure()
-plt.imshow(img, cmap="gray")
-plt.show()
+#Otvoriti LV3_Biondic_zad_3
